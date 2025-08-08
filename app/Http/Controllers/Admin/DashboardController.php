05f9b2178d12a1511e9758079bde865e58e1262e
Vikrialva <vikrialvapratama27@gmail.com>
2025-08-08 13:53:34 +0700

    hallo admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-     public function index()
-    {
-        return view('admin.dashboard');
-    }
+    public function index()
+{
+    $userName = Auth::user()->name;
+    $email = Auth::user()->email;
+
+    return view('admin.dashboard', [
+        'userName' => $userName,
+        'email' => $email
+    ]);
+}
 }
